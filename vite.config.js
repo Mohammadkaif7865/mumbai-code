ce8b1c650cf50505b43002e7fbdf74129996dc4f
sib-vishal <vishal.maurya@sibinfo.tech>
2024-05-21 12:13:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/digital-marketing-services-in-dallas/',
+  base: '/digital-marketing-services-in-mumbai/',
   plugins: [react()],
 });
