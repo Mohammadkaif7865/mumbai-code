b09f07c0c09fefdb0110809d510407bcccc57b92
sib-vishal <vishal.maurya@sibinfo.tech>
2024-06-07 16:16:13 +0530

    commit mumbai done

--- src/components/Home/HeroForm.jsx
+++ src/components/Home/HeroForm.jsx
@@ -1,17 +1,32 @@
 import React, { useState } from "react";
-import Services from "./Services";
-import axios from "axios";
-import "./heroform.css";
 import { toast } from "react-toastify";
+import Services from "./Services";
+import "./heroform.css";
 
 const HeroForm = ({ oneline }) => {
+ 
+  const option = [
+    "Website Design",
+    "Website Design",
+    "Website Development",
+    "Digital Marketing",
+    "PPC",
+    "SEO",
+    "SMO",
+    "Software Development",
+    "Graphic Design",
+    "Mobile App Development",
+    "Offshore Outsourcing",
+  ];
   const [formData, setFormData] = useState({
     name: "",
     phoneNo: "",
+    email: "",
     companyName: "",
     websiteUrl: "",
-    email: "",
-    fromWhere: "mumbai",
+    service: "",
+    message: "",
+    fromWhere: "DMS - Mumbai",
   });
 
   const handleChange = (e) => {
@@ -24,17 +39,18 @@ const HeroForm = ({ oneline }) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // console.log("Form Data:", formData);
     const data = {
       name: formData.name,
       company: formData.companyName,
-      website: formData.websiteUrl,
       email: formData.email,
+      website: formData.websiteUrl,
       phone: formData.phoneNo,
+      service: formData.service,
+      message: formData.message,
       fromWhere: formData.fromWhere,
     };
 
-    // console.log(data);
+    console.log(data);
 
     try {
       const API_TOKEN = "FgRCHG4OVv8Z1BcrjExKJcqspvTsUTCe";
@@ -48,23 +64,23 @@ const HeroForm = ({ oneline }) => {
         body: JSON.stringify(data),
       });
 
-      console.log(response.data);
-      if (response.status == 200) {
-        toast.success("Email Send Successfully!");
+      if (response.status === 200) {
+        toast.success("Email Sent Successfully!");
         setFormData({
           name: "",
+          phoneNo: "",
+          email: "",
           companyName: "",
           websiteUrl: "",
-          email: "",
-          phoneNo: "",
+          service: "",
+          message: "",
         });
 
         setTimeout(() => {
           window.location.href = "https://sibinfotech.com/thanks";
         }, 2000);
-      }
-      if (!response.status == 200) {
-        return toast.error("Somthing went wrong!");
+      } else {
+        toast.error("Something went wrong!");
       }
     } catch (error) {
       console.error("Error:", error);
@@ -73,7 +89,6 @@ const HeroForm = ({ oneline }) => {
 
   return (
     <>
-   
       <div
         className={`w-[100%] bg-white rounded-2xl   ${
           oneline ? "py-12 " : "p-5"
@@ -92,10 +107,11 @@ const HeroForm = ({ oneline }) => {
                 className=""
                 type="text"
                 name="name"
-                inputMode="text"
                 value={formData.name}
                 onChange={handleChange}
                 required
+                // Keyboard type for name
+                inputMode="text"
               />
               <label htmlFor="name" className="">
                 Name
@@ -105,63 +121,97 @@ const HeroForm = ({ oneline }) => {
               <input
                 className=""
                 type="text"
+                name="companyName"
+                value={formData.companyName}
+                onChange={handleChange}
+                required
+                // Keyboard type for company name
+                inputMode="text"
+              />
+              <label htmlFor="companyName" className="">
+                Company Name
+              </label>
+            </div>
+          </div>
+          <div className={`grid   ${oneline ? "" : "grid-cols-2 gap-4 "}`}>
+            <div class="user-box mt-[23px] ">
+              <input
+                className=""
+                type="text"
+                name="websiteUrl"
+                value={formData.websiteUrl}
+                onChange={handleChange}
+                required
+                // Keyboard type for name
+                inputMode="text"
+              />
+              <label htmlFor="websiteUrl" className="">
+                Website Url
+              </label>
+            </div>
+            <div class="user-box mt-[23px] ">
+              <input
+                className=""
+                type="text"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+                // Keyboard type for company name
+                inputMode="text"
+              />
+              <label htmlFor="email" className="">
+                Email
+              </label>
+            </div>
+          </div>
+
+          <div className={`grid   ${oneline ? "" : "grid-cols-2 gap-4 "}`}>
+            <div class="user-box mt-[23px] ">
+              <input
+                className=""
+                type="tel"
                 name="phoneNo"
-                inputMode="tel"
                 value={formData.phoneNo}
                 onChange={handleChange}
                 required
+                // Keyboard type for phone number
+                inputMode="tel"
               />
               <label htmlFor="phoneNo" className="">
                 Phone No
               </label>
             </div>
+
+            {/* dropdown */}
+            <div className="mt-[23px] text-sm">
+              <select
+                className=" mt-[2px] outline-none w-full relative border-b border-[#9b9b9b] pb-[7px] "
+                name="service"
+                value={formData.service}
+                onChange={handleChange}
+                required
+              >
+                {option.map((option, index) => (
+                  <option className="w-full px-2" key={index} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </select>
+            </div>
           </div>
-          <div class="user-box mt-[23px] ">
-            <input
-              className=""
-              type="text"
-              name="companyName"
-              inputMode="text"
-              value={formData.companyName}
+          <div className="relative mb-2 mt-[22px]">
+            <textarea
+              id="message"
+              name="message"
+              placeholder="Message"
+              value={formData.message}
               onChange={handleChange}
-              required
-            />
-            <label htmlFor="companyName" className="">
-              Company Name
-            </label>
+              className="w-full bg-white text-sm rounded border  border-[#9b9b9b] focus:border-blue-500   h-16 outline-none text-[#9b9b9b] py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out placeholder:text-[#9b9b9b]"
+            ></textarea>
           </div>
 
-          <div class="user-box mt-[23px] ">
-            <input
-              className=""
-              type="text"
-              name="websiteUrl"
-              inputMode="url"
-              value={formData.websiteUrl}
-              onChange={handleChange}
-              required
-            />
-            <label htmlFor="websiteUrl" className="">
-              Website Url
-            </label>
-          </div>
-
-          <div class="user-box mt-[23px] ">
-            <input
-              className=""
-              type="text"
-              name="email"
-              inputMode="email"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
-            <label htmlFor="email" className="">
-              Email
-            </label>
-          </div>
-
-          <button className="py-3 rounded-xl bg-[#e31a20] mt-5 w-full  font-poppins text-white text-[16px] md:text-xl ">
+          <button className="py-2 rounded-xl bg-[#e31a20]  w-full  font-poppins text-white text-[16px] md:text-xl ">
             SEND REQUEST
           </button>
         </form>
